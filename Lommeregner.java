71bef6732fcb6885a833aca093fee646c28fd289
casper madsen <casp589b@elev.techcollege.dk>
2023-03-01 09:56:20 +0100

    tilføjet dokumentation til koden

--- Lommeregner.java
+++ Lommeregner.java
@@ -2,68 +2,73 @@ import java.util.Scanner;
 
 public class Lommeregner {
 	
-	public enum calculatorMode{
-		
-		ADD("+"),
-		SUBTRACT("-"),
-		MULTIPLY("*"),
-		DIVISION("/");
-		
-		private final String symbol;
-		 calculatorMode(String symbol){
-			
-			this.symbol = symbol;
-		}
-		
-		@Override
-		public String toString(){
-			 
-			 return symbol;
-		}
-	}
-	public static double calculator(double a, double b, calculatorMode mode){
-		
-		return switch (mode){
-			
-			case ADD -> a + b;
-			
-			case SUBTRACT -> a - b;
-			
-			case MULTIPLY -> a*b;
-			
-			case DIVISION -> a/b;
-			
-			
-		};
-	}
-	
 	public static void main(String[] args) {
 		
+		// scanner bruges til og læse input fra brugeren
 		Scanner scanner = new Scanner(System.in);
 		
+		// spørg brugeren om det først tal
 		System.out.println("tal 1");
 		double a = scanner.nextDouble();
 		
+		// spørg brugeren om det andet tal
 		System.out.println("tal 2");
 		double b = scanner.nextDouble();
 		
+		// spørg brugeren om tilstand
 		System.out.println("vælg en tilstand");
 		System.out.println("1: +");
 		System.out.println("2: -");
 		System.out.println("3: *");
 		System.out.println("4: /");
 		int index = scanner.nextInt();
-		calculatorMode mode = calculatorMode.values()[index -1];
+		CalculatorMode mode = CalculatorMode.values()[index - 1];
 		
-		double calculation = calculator(a, b, mode);
-		System.out.printf("%.2f %s %.2f = %.2f \n", a, mode.toString(), b, calculation );
+		// bruges til at formater resultat til 2 decimaler
+		double calculation = calculate(a, b, mode);
+		System.out.printf("%.2f %s %.2f = %.2f \n", a, mode, b, calculation);
+	}
+	
+	/**
+	 * Udregner resultatet af a og b med udvalgt regningsmetode.
+	 *
+	 * @param a    Først tal.
+	 * @param b    Andet tal.
+	 * @param mode regnemetoden
+	 * @return resultatet af a og b med udvalgt regningsmetode
+	 */
+	public static double calculate(double a, double b, CalculatorMode mode) {
 		
+		return switch (mode) {
+			
+			case ADD -> a + b;
+			case SUBTRACT -> a - b;
+			case MULTIPLY -> a * b;
+			case DIVISION -> a / b;
+		};
+	}
+	
+	/**
+	 * Regne metoden som bruges til udregninger.
+	 *
+	 * @see #calculate(double, double, CalculatorMode)
+	 */
+	public enum CalculatorMode {
 		
+		ADD("+"), SUBTRACT("-"), MULTIPLY("*"), DIVISION("/");
 		
+		private final String symbol;
 		
+		CalculatorMode(String symbol) {
+			
+			this.symbol = symbol;
+		}
 		
-		
-		
+		@Override
+		public String toString() {
+			
+			return symbol;
+		}
 	}
 }
 
